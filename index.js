b21ea41472bfc809dec234aa72e104ee95d12599
Tobechi <tobechichukwuleta125@gmail.com>
2022-01-30 19:18:19 -0800

    Test for Heroku

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.use(express.json())
 
 // Calling the endpoints
 app.use('/movies', movieRoutes)
-app.use('/characters', characterRoutes)
-app.use('/comments', commentRoutes)
+// app.use('/characters', characterRoutes)
+// app.use('/comments', commentRoutes)
 app.get('/', (req, res) => {
     res.json({ message: "Welcome Aboard" })
 })
@@ -21,7 +21,7 @@ app.get('*', (req, res) => {
     res.status(404).json({ message: "Guess you are lost. Just head to the home page" })
 })
 
-const port = 2022 || process.env.PORT
+const port = process.env.PORT || 2022
 app.listen(port, () => {
     console.log(`Hey star fans... This server is up and running on port ${port}`)
 }) 
