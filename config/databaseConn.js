981c2bc4e7260e9d85eee7aa25192c0ec50c3c42
Tobechi <tobechichukwuleta125@gmail.com>
2022-01-31 10:03:48 -0800

    Fix for live database

--- config/databaseConn.js
+++ config/databaseConn.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize")
 require("dotenv").config()
 
-const config = {
+const Devconfig = {
     dialect: 'postgres', 
     pool: {
         max: 5,
@@ -11,13 +11,29 @@ const config = {
       }
 }
 
+const Prodconfig = { 
+    dialect: 'postgres', 
+    dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+        }
+    },
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+}
+
 let sequelize 
 
 if(process.env.DATABASE_URL){
-    sequelize = new Sequelize(process.env.DATABASE_URL, config)
+    sequelize = new Sequelize(process.env.DATABASE_URL, Prodconfig)
 }
 else{
-    sequelize = new Sequelize(process.env.PG_DATABASE, process.env.PG_USER, process.env.PG_PASSWORD, config)
+    sequelize = new Sequelize(process.env.PG_DATABASE, process.env.PG_USER, process.env.PG_PASSWORD, Devconfig)
 }
 
 
--- models/Comment.js
+++ models/Comment.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize")
 const sequelize = require("../config/databaseConn")
  
-const Comment = sequelize.define('comment', {
+const Comment = sequelize.define('comments', {
     id: {
         type: Sequelize.INTEGER,
         autoIncrement: true,
@@ -15,11 +15,23 @@ const Comment = sequelize.define('comment', {
     comment: {
         type: Sequelize.TEXT,
         allowNull: false
-    }, 
+    },  
     public_ip: {
         type: Sequelize.TEXT,
         allowNull: false
+    },
+    createdAt: {
+        type: Sequelize.DATE,
+        field: 'created_at'
+    },
+    updatedAt: {
+        type: Sequelize.DATE,
+        field: 'updated_at'
     }
+}, {
+    timestamps: true, 
+    underscored: true,
+    freezeTableName: true
 })
 
-module.exports = Comment  
+module.exports = Comment   
